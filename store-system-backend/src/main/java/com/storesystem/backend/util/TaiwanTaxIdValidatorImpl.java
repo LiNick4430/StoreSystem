3f778c51f6ea50abfca271acaf584ae8f949c864
LiaNick <poy810106@gmail.com>
2026-01-15 12:15:12 +0800

    註解 修正

--- store-system-backend/src/main/java/com/storesystem/backend/util/TaiwanTaxIdValidatorImpl.java
+++ store-system-backend/src/main/java/com/storesystem/backend/util/TaiwanTaxIdValidatorImpl.java
@@ -5,12 +5,13 @@ import com.storesystem.backend.annotation.TaiwanTaxId;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+/** 用於 BEAN VALIDATION 自定註解 的 方法 */
 public class TaiwanTaxIdValidatorImpl implements ConstraintValidator<TaiwanTaxId, String>{
 
 	@Override
 	public boolean isValid(String value, ConstraintValidatorContext context) {
 
-		// 如果是 null 交給 @NotBlank 控管 回傳 true 防止重複報錯
+		// 給 @NotBlank 控管 回傳 true 防止重複報錯
 		if (value == null || value.isBlank()) {
 			return true;
 		}
